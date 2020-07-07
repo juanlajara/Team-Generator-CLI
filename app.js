f9b6b6dbefc593a4f6a389f1249260196a8e79d1
juanlajara <juanlajara001@gmail.com>
2020-07-06 21:49:44 -0500

    +Test Adding a file

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ const render = require("./lib/htmlRenderer");
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
+// Test
 function validation(value) {
 	if (value != "") {
 		return true;
